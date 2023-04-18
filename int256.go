25a51881da3fc5bf91eeed74d56db77f27636138
linhbkhn95 <trinhducbaolinh@gmail.com>
2023-04-18 15:12:53 +0700

    add some functions

--- int256.go
+++ int256.go
@@ -1,6 +1,12 @@
 package int256
 
-import "github.com/holiman/uint256"
+import (
+	"math/big"
+
+	"github.com/holiman/uint256"
+)
+
+// var one = uint256.NewInt(1)
 
 type Int struct {
 	abs *uint256.Int
@@ -45,6 +51,43 @@ func (z *Int) SetUint64(x uint64) *Int {
 func NewInt(x int64) *Int {
 	return new(Int).SetInt64(x)
 }
+
+// SetUint64 sets z to x and returns z.
+func (z *Int) SetString(s string) (*Int, error) {
+	neg := false
+	// Remove max one leading +
+	if len(s) > 0 && s[0] == '+' {
+		neg = false
+		s = s[1:]
+	}
+
+	if len(s) > 0 && s[0] == '-' {
+		neg = true
+		s = s[1:]
+	}
+
+	abs, err := uint256.FromDecimal(s)
+	if err != nil {
+		return nil, err
+	}
+	return &Int{
+		abs,
+		neg,
+	}, nil
+}
+
+// // setFromScanner implements SetString given an io.ByteScanner.
+// // For documentation see comments of SetString.
+// func (z *Int) setFromScanner(r io.ByteScanner, base int) (*Int, bool) {
+// 	if _, _, err := z.scan(r, base); err != nil {
+// 		return nil, false
+// 	}
+// 	// entire content must have been consumed
+// 	if _, err := r.ReadByte(); err != io.EOF {
+// 		return nil, false
+// 	}
+// 	return z, true // err == io.EOF => scan consumed all content of r
+// }
 
 func (z *Int) Add(x, y *Int) *Int {
 	neg := x.neg
@@ -165,3 +208,66 @@ func (x *Int) Cmp(y *Int) (r int) {
 	}
 	return
 }
+
+// Exp sets z = x**y mod |m| (i.e. the sign of m is ignored), and returns z.
+// If m == nil or m == 0, z = x**y unless y <= 0 then z = 1. If m != 0, y < 0,
+// and x and m are not relatively prime, z is unchanged and nil is returned.
+//
+// Modular exponentiation of inputs of a particular size is not a
+// cryptographically constant-time operation.
+func (z *Int) Exp(x, y, m *Int) *Int {
+	if !x.neg && !y.neg {
+		z.neg = false
+		z.abs.Exp(x.abs, y.abs)
+	}
+	// TODO: implement
+	big := new(big.Int).Exp(x.ToBig(), y.ToBig(), m.ToBig())
+	z, _ = FromBig(big)
+	return z
+}
+
+func (z *Int) Div(x, y *Int) *Int {
+	z.abs.Div(x.abs, y.abs)
+	if x.neg == y.neg {
+		z.neg = false
+	}
+	z.neg = true
+	return z
+}
+
+// Lsh sets z = x << n and returns z.
+func (z *Int) Lsh(x *Int, n uint) *Int {
+	z.abs = z.abs.Lsh(x.abs, n)
+	z.neg = x.neg
+	return z
+}
+
+// // Or sets z = x | y and returns z.
+// func (z *Int) Or(x, y *Int) *Int {
+// 	if x.neg == y.neg {
+// 		if x.neg {
+// 			// (-x) | (-y) == ^(x-1) | ^(y-1) == ^((x-1) & (y-1)) == -(((x-1) & (y-1)) + 1)
+// 			x1 := new(uint256.Int).Sub(x.abs, one)
+// 			y1 := new(uint256.Int).Sub(y.abs, one)
+// 			z.abs = z.abs.Add(z.abs.And(x1, y1), one)
+// 			z.neg = true // z cannot be zero if x and y are negative
+// 			return z
+// 		}
+
+// 		// x | y == x | y
+// 		z.abs = z.abs.Or(x.abs, y.abs)
+// 		z.neg = false
+// 		return z
+// 	}
+
+// 	// x.neg != y.neg
+// 	if x.neg {
+// 		x, y = y, x // | is symmetric
+// 	}
+
+// 	// x | (-y) == x | ^(y-1) == ^((y-1) &^ x) == -(^((y-1) &^ x) + 1)
+// 	y1 := new(uint256.Int).Sub(y.abs, one)
+// 	z.abs = z.abs.Add(z.abs.andNot(y1, x.abs), one)
+// 	z.neg = true // z cannot be zero if one of x or y is negative
+// 	return z
+// }
